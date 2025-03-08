6c81602a05aa2e92c15ff87b65919d539271f07c
coderanik <anikdas210605@gmail.com>
2025-03-09 02:21:55 +0530

    feat:developed authentication

---
+++ routes/authRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  verify,
+  verifyEmail,
+  isAuthenticated,
+  sendResetOtp,
+  resetPassword,
+} from "../controller/authController.js";
+import userAuth from "../middleware/userAuth.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/sentverifyotp", userAuth, verify);
+router.post("/verifyaccount", userAuth, verifyEmail);
+router.post("/isAuth", userAuth, isAuthenticated);
+router.post("/sendresetotp", sendResetOtp);
+router.post("/resetpassword", resetPassword);
+
+export default router;
